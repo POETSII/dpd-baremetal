bfc528550233666cfac843ada884d998ad2d8d09
STFleming <sf306@ic.ac.uk>
2018-11-09 15:40:37 +0000

    Added some double precision tests to compare against the fixed point version

--- tests/fixed_ap_tests.cpp
+++ tests/fixed_ap_tests.cpp
@@ -1,6 +1,7 @@
 // A test file for the fixed point arithmetic stuff
 #include "fixed_ap.h"
 #include "vec3d.h"
+#include "vec3d_double.h"
 #include <stdio.h>
 #include <math.h>
 
@@ -10,6 +11,10 @@ int main() {
 
   int t = 0;
   
+  printf("----------------------\n");
+  printf("  fixed point library\n");
+  printf("----------------------\n");
+
   fixap<uint16_t, 8> a1(0.4);  
   printf("<16,8> 0.4 = %.4f\n", (float)a1);
 
@@ -18,14 +23,17 @@ int main() {
 
   fixap<uint16_t, 13> a3(0.00021);
   printf("<16,13> 0.00021 = %.4f\n", (float)a3);
+  printf("\n");
 
   fixap<uint16_t, 13> a4(0.03);
   fixap<uint16_t, 13> b4(0.7);
   printf("<16,13>0.03 * <16,13>0.7 = %.4f\n", (float)(a4 * b4)); 
+  printf("<dp> 0.03 * 0.7 = %f\n\n", (double)(0.03 * 0.7));
 
   fixap<uint16_t, 13> a5(0.7);
   fixap<uint16_t, 13> b5(0.35);
-  printf("<16,13>0.7 * <16,13>0.35 = %.4f\n", (float)(a5 / b5)); 
+  printf("<16,13>0.7 / <16,13>0.35 = %.4f\n", (float)(a5 / b5)); 
+  printf("<dp> 0.7 / 0.35 = %f\n\n", (double)(0.7 / 0.35));
 
   printf("testing the square roots\n");
   fixap<uint16_t, 14> a7(1.5);
@@ -46,60 +54,81 @@ int main() {
   fixap<uint32_t, 30> a11(1.5);
   printf("<32,30> inv_sqrt(1.5) = %.4f\n", (float)a11.inv_sqrt(20)); 
 
-  fixap<uint32_t, 30> a11_a(1.5546);
-  printf("<32,13> inv_sqrt(1.5546) = %.4f\n", (float)a11_a.inv_sqrt(25)); 
+  printf("<dp> inv_sqrt(1.5) = %f\n\n", (double)(1.0/sqrt(1.5)));
 
   fixap<uint16_t, 14> a12(1.5);
   printf("<16,14> sqrt(1.5) = %.4f\n", (float)a12.sqrt(20)); 
+  printf("<dp> inv_sqrt(1.5) = %f\n\n", (double)(sqrt(1.5)));
 
   // half test
   fixap<uint32_t, 30> ht1(2.4);
-  printf("<32,30> 2.4/2 = %.4f\n", (float)ht1.half());
+  printf("<32,30> 2.4/2 = %.4f\n\n", (float)ht1.half());
 
-  // -------------------------------
-  //  vector testing
-  // -------------------------------
+  printf("----------------------\n");
+  printf("  vector testing      \n");
+  printf("----------------------\n\n");
 
   vec3d<int16_t, 13> vec_a1(0.67, 0.56, 0.89);
   vec3d<int16_t, 13> vec_b1(1.2, 0.68, 0.92);
+  vec3d_double vec_a1_dp(0.67, 0.56, 0.89);
+  vec3d_double vec_b1_dp(1.2, 0.68, 0.92);
+  vec3d_double res(0.0,0.0,0.0);
 
   // vector - vector addition
   vec3d<int16_t, 13> vec_out1 = vec_a1 + vec_b1;
   printf("%s + %s = %s\n", vec_a1.str().c_str(), vec_b1.str().c_str(),vec_out1.str().c_str());
+  res = vec_a1_dp + vec_b1_dp;
+  printf("<dp> = %s\n\n", res.str().c_str());
 
   vec3d<int16_t, 13> vec_out2 = vec_a1 + 0.65;
   printf("%s + 0.65 = %s\n", vec_a1.str().c_str(), vec_out2.str().c_str());
 
   vec3d<int16_t, 13> vec_out3 = vec_a1 + fixap<int16_t, 13>(0.65);
-  printf("%s + 0.65 = %s\n", vec_a1.str().c_str(), vec_out3.str().c_str());
+  printf("%s + <16,13>0.65 = %s\n", vec_a1.str().c_str(), vec_out3.str().c_str());
+  res = vec_a1_dp + 0.65;
+  printf("<dp> = %s\n\n", res.str().c_str());
 
   // vector - vector subtraction
   vec3d<int16_t, 13> vec_out4 = vec_a1 - vec_b1;
   printf("%s - %s = %s\n", vec_a1.str().c_str(), vec_b1.str().c_str(),vec_out4.str().c_str());
+  res = vec_a1_dp - vec_b1_dp;
+  printf("<dp> = %s\n\n", res.str().c_str());
 
   vec3d<int16_t, 13> vec_out5 = vec_a1 - 0.65;
   printf("%s - 0.65 = %s\n", vec_a1.str().c_str(), vec_out5.str().c_str());
 
   vec3d<int16_t, 13> vec_out6 = vec_a1 - fixap<int16_t, 13>(0.65);
-  printf("%s - 0.65 = %s\n", vec_a1.str().c_str(), vec_out6.str().c_str());
+  printf("%s - <16,13> 0.65 = %s\n", vec_a1.str().c_str(), vec_out6.str().c_str());
+  res = vec_a1_dp - 0.65;
+  printf("<dp> = %s\n\n", res.str().c_str());
 
   // scalar mult and div
   vec3d<int16_t, 13> vec_out7 = vec_a1 * 0.05;
   printf("%s * 0.05 = %s\n", vec_a1.str().c_str(), vec_out7.str().c_str());
+  res = vec_a1_dp * 0.05;
+  printf("<dp> = %s\n\n", res.str().c_str());
 
   vec3d<int16_t, 13> vec_out8 = vec_a1 / 1.2;
   printf("%s / 1.2 = %s\n", vec_a1.str().c_str(), vec_out8.str().c_str());
+  res = vec_a1_dp / 1.2;
+  printf("<dp> = %s\n\n", res.str().c_str());
 
   // magnitude
   printf("mag(%s) = %.4f\n", vec_a1.str().c_str(), (float)vec_a1.convert_u16().mag());
+  double res_dp = vec_a1_dp.mag();
+  printf("<dp> = %f\n\n", res_dp);
 
   // dot product
   fixap<int16_t, 13> vec_out9 = vec_a1.dot(vec_b1);
   printf("%s . %s = %.4f\n", vec_a1.str().c_str(), vec_b1.str().c_str(), (float)vec_out9);
+  res_dp = vec_a1_dp.dot(vec_b1_dp);
+  printf("<dp> = %f\n\n", res_dp);
 
   // distance 
   fixap<int16_t, 13> vec_out10 = vec_a1.dist(vec_b1);
   printf("distance(%s <-> %s) = %.4f\n", vec_a1.str().c_str(), vec_b1.str().c_str(), (float)vec_out10);
+  res_dp = vec_a1_dp.dist(vec_b1_dp);
+  printf("<dp> = %f\n\n", res_dp);
 
   printf("\n-----------------------------\n");
   if(t == 1) {
