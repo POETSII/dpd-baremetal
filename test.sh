0cb06587dd9dce570bbe8a9db32f0d9cd098bd6e
Jonny Beaumont <jonathan.r.beaumont@gmail.com>
2019-10-01 09:48:35 +0000

    Tests will now be re-run if they fail

--- test.sh
+++ test.sh
@@ -3,23 +3,28 @@
 cd ./bin
 
 i=0
-s=24
-while [[ $s -ge 3 ]]
+s=33
+while [[ $s -le 50 ]]
 do
+    num=0
     while [[ $i -lt 5 ]]
     do
         if [[ $i -eq 0 ]] ; then
-            ./run $s 1
-        else
-            ./run $s
+            if [[ $num -eq 0 ]] ; then
+                echo -n "$s, " >> ../timing_results.csv
+                num=1
+            fi
         fi
-        if [[ $i -lt 4 ]] ; then
+        timeout 200s ./run $s -k
+        if [[ $? -gt 0 ]] ; then
+            i=$((i - 1))
+        elif [[ $i -lt 4 ]] ; then
             echo -n ", " >> ../timing_results.csv
         fi
         i=$((i + 1))
-        sleep 5s
+        sleep 30s
     done
     i=0
     echo >> ../timing_results.csv
-    s=$((s - 1))
+    s=$((s + 1))
 done
