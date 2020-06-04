8b556f05f685cf141d2887dd607b72a07076d6d7
Jonny Beaumont <jonathan.r.beaumont@gmail.com>
2020-06-04 17:48:16 +0100

    Added some rdf analysis

---
+++ utils/analysis/cell.py
@@ -0,0 +1,126 @@
+import csv
+import math
+
+class Cell:
+
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.beads = []
+
+    def getNeighbourLoc(self, n_x, n_y, n_z, cells, vol_width):
+        r_x = self.x
+        r_y = self.y
+        r_z = self.z
+
+        if n_x == -1:
+            if self.x == 0:
+                r_x = vol_width - 1
+            else:
+                r_x = self.x - 1
+        if n_x == 1:
+            if self.x == vol_width - 1:
+                r_x = 0
+            else:
+                r_x = self.x + 1
+
+        if n_y == -1:
+            if self.y == 0:
+                r_y = vol_width - 1
+            else:
+                r_y = self.y - 1
+        if n_y == 1:
+            if self.y == vol_width - 1:
+                r_y = 0
+            else:
+                r_y = self.y + 1
+
+        if n_z == -1:
+            if self.z == 0:
+                r_z = vol_width - 1
+            else:
+                r_z = self.z - 1
+        if n_z == 1:
+            if self.z == vol_width - 1:
+                r_z = 0
+            else:
+                r_z = self.z + 1
+
+        return cells[r_x][r_y][r_z]
+
+class Bead:
+
+    def __init__(self, id, type):
+        self.id = id
+        self.type = type
+        self.pos = Vector(0.0, 0.0, 0.0)
+        self.velo = Vector(0.0, 0.0, 0.0)
+
+class Vector:
+
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def subtractVec(self, pos):
+        return Vector(self.x - pos.x, self.y - pos.y, self.z - pos.z)
+
+    def multiplyVec(self, pos):
+        return Vector(self.x * pos.x, self.y * pos.y, self.z * pos.z)
+
+    def mag(self):
+        return math.sqrt(self.x * self.x + self.y * self.y + self.z * self.z)
+
+    def getEuclideanDistance(self, pos):
+        sub = self.subtractVec(pos)
+        return sub.mag()
+
+def getUniverseAtTimestepForWidth(timestep, vol_width):
+    cells = [[[0 for _ in range(vol_width)] for _ in range(vol_width)] for _ in range(vol_width)]
+
+    for x in range(0, vol_width):
+        for y in range(0, vol_width):
+            for z in range(0, vol_width):
+                c = Cell(x, y, z)
+                cells[x][y][z] = c
+
+    filepath = "../../3_full_bead_info/3_3_3_time_" + str(timestep) + ".csv"
+
+    with open(filepath) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            # Get bead info from file
+            bead_id = int(row[0].strip())
+            bead_type = int(row[1].strip())
+            cell_x = int(row[2].strip())
+            cell_y = int(row[3].strip())
+            cell_z = int(row[4].strip())
+            pos_x = float(row[5].strip())
+            pos_y = float(row[6].strip())
+            pos_z = float(row[7].strip())
+            pos = Vector(pos_x, pos_y, pos_z)
+            vel_x = float(row[8].strip())
+            vel_y = float(row[9].strip())
+            vel_z = float(row[10].strip())
+            vel = Vector(vel_x, vel_y, vel_z)
+
+            # Generate bead
+            bead = Bead(bead_id, bead_type)
+            bead.pos = pos
+            bead.velo = vel
+
+            # Add to cell
+            cells[cell_x][cell_y][cell_z].beads.append(bead)
+
+    return cells
+
+def getTotalBeads(cells):
+    totalBeads = 0
+    for x in cells:
+        for y in x:
+            for z in y:
+                totalBeads = totalBeads + len(z.beads)
+    return totalBeads
+
---
+++ utils/analysis/rdf.py
@@ -0,0 +1,58 @@
+from cell import *
+import math
+import numpy
+
+vol_width = 3 # Constant for each run
+number_density = 3
+
+max_timestep = 10000
+min_timestep = 1
+
+rmax = 1
+dr = 0.1
+
+outputFile = str(vol_width) + "_rdf_dr_" + str(dr) +".csv"
+f = open(outputFile, "w")
+
+# Prep top line of csv
+f.write(",0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9\n")
+
+# For each timestep calculate the RDF for 10 shells between radius 0 and 1
+for timestep in range(min_timestep, max_timestep):
+    print("Timestep: " + str(timestep))
+    f.write(str(timestep) + ",")
+    # Get universe for this timestep from files
+    cells = getUniverseAtTimestepForWidth(timestep, vol_width)
+    total_beads = getTotalBeads(cells)
+    # Iterate over all values of r in rs
+    r = 0.0
+    while r < (rmax - dr):
+        print(r)
+        beads_within_shells = 0
+        max_r = r + dr;
+        for x in range(0, vol_width):
+            for y in range(0, vol_width):
+                for z in range(0, vol_width):
+                    c = cells[x][y][z]
+                    for n_x in [-1, 0, 1]:
+                        for n_y in [-1, 0, 1]:
+                            for n_z in [-1, 0, 1]:
+                                n = c.getNeighbourLoc(n_x, n_y, n_z, cells, vol_width)
+                                for i in c.beads:
+                                    for j in n.beads:
+                                        if (i.id != j.id):
+                                            adj_j = Vector(j.pos.x + n_x, j.pos.y + n_y, j.pos.z + n_z)
+                                            dist = i.pos.getEuclideanDistance(adj_j)
+                                            if dist > r and dist < max_r:
+                                                beads_within_shells = beads_within_shells + 1
+        avg = float(beads_within_shells) / float(total_beads)
+        volume = (4 / 3) * numpy.pi * ((r + dr) ** 3 - r ** 3)
+        g1 = avg / volume
+        g2 = g1 / number_density
+        f.write(str(g2) + ",")
+        # Next shell
+        r = r + dr
+    # End of timestep
+    f.write("\n")
+
+print("Finished")
