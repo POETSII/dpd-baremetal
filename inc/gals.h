396c5fda76d6793a01a9dd600c6d7483eec3052e
Jonny Beaumont <jonathan.r.beaumont@gmail.com>
2020-11-24 13:38:05 +0000

    Ading FLOAT_ONLY optio to GALS. Tidying Vector3D if its active

--- inc/gals.h
+++ inc/gals.h
@@ -81,7 +81,11 @@ struct DPDState {
     uint32_t sentslot; // a bitmap of which bead slot has not been sent from yet
     uint32_t newBeadMap;
     bead_t bead_slot[MAX_BEADS]; // at most we have five beads per device
-    Vector3D<int32_t> force_slot[MAX_BEADS]; // at most 5 beads -- force for each bead
+#ifdef FLOAT_ONLY
+    Vector3D<float> force_slot[MAX_BEADS];
+#else
+    Vector3D<int32_t> force_slot[MAX_BEADS]; // force for each bead
+#endif
 #ifdef BETTER_VERLET
     Vector3D<ptype> old_velo[MAX_BEADS]; // Store old velocites for verlet
 #endif
@@ -590,9 +594,12 @@ struct DPDDevice : PDevice<DPDState, None, DPDMessage> {
                             Vector3D<ptype> f;
                             f.set(r.x, r.y, r.z);
                         #endif
-
+                        #ifdef FLOAT_ONLY
+                            s->force_slot[ci] = s->force_slot[ci] + f;
+                        #else
                             Vector3D<int32_t> x = f.floatToFixed();
                             s->force_slot[ci] = s->force_slot[ci] + x;
+                        #endif
                     #ifndef ONE_BY_ONE
                         }
                     #endif
