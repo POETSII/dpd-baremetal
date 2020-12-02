af23d62528d1fd7aa6b5df9ad99aae7472ec628b
Jonny Beaumont <jonathan.r.beaumont@gmail.com>
2020-12-02 13:57:41 +0000

    Added convert script from JSON states to CSV for restart run

---
+++ utils/convert-json-to-restart.py
@@ -0,0 +1,34 @@
+import json
+
+volLength = 25
+timestep = 4200001
+in_filepath = "../25_bond_frames/state_" + str(timestep) + ".json"
+out_filepath = "../restart_4200001.csv"
+f = open(out_filepath, "w+")
+
+print("Writing CSV file preamble")
+f.write("VOLUME DIMENSIONS\n")
+f.write(str(volLength) + ", " + str(volLength) + ", " + str(volLength) + "\n")
+f.write("TIMESTEP\n")
+f.write(str(timestep) + "\n")
+f.write("BONDED BEAD IDS\n")
+f.write("BEAD POSITIONS\n")
+
+print("Loading JSON state file")
+count = 1
+with open(in_filepath) as json_file:
+    data = json.load(json_file) # Load JSON file
+    for b in data["beads"]: # For each bead in the file
+        print("Converting bead " + str(count) + " of " + str(len(data["beads"])), end = "\r")
+        bead_id = b["id"]
+        bead_type = b["type"]
+        x_pos = b["x"]
+        y_pos = b["y"]
+        z_pos = b["z"]
+        x_vel = b["vx"]
+        y_vel = b["vy"]
+        z_vel = b["vz"]
+        f.write(str(bead_id) + ", " + str(bead_type) + ", " + str(x_pos) + ", " + str(y_pos) + ", " + str(z_pos) + ", " + str(x_vel) + ", " + str(y_vel) + ", " + str(z_vel) + "\n")
+
+f.close()
+print("\nComplete")
