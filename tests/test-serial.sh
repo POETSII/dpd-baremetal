96a5c473e61e1b3fa37e8710731cab8baca0ff2e
Jonny Beaumont <jonathan.r.beaumont@gmail.com>
2021-05-06 16:02:16 +0000

    Added serial specific tests

---
+++ tests/test-serial.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+clear
+clear
+
+cd ..
+make clean
+make clean-tinsel
+
+tests=(test-serial
+test-serial-betterverlet
+test-serial-dtchange
+test-serial-doublesqrt
+test-serial-floatonly
+test-serial-reducedlocalcalcs
+test-serial-singleforceloop
+test-serial-large
+test-serial-large-betterverlet
+test-serial-large-dtchange
+test-serial-large-doublesqrt
+test-serial-large-floatonly
+test-serial-large-reducedlocalcalcs
+test-serial-large-singleforceloop
+test-serial-bonds
+test-serial-bonds-betterverlet
+test-serial-bonds-dtchange
+test-serial-bonds-doublesqrt
+test-serial-bonds-floatonly
+test-serial-bonds-reducedlocalcalcs
+test-serial-bonds-singleforceloop
+)
+
+function build_and_run {
+    make clean
+    echo
+    echo "======================= RUNNING $1 ======================="
+    make $1
+    cd bin
+    timeout 60s ./test
+    result=$?
+    if [[ $result -ne 0 ]] ; then
+      exit $result
+    fi
+    cd ..
+    sleep 10s
+}
+
+for t in ${tests[@]}; do
+  build_and_run $t
+done
+
+echo "ALL TESTING PASSED"
+echo "====================================================================="
+
+cd tests
